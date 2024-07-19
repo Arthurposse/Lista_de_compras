e748e5760405512f8099c67fb5ddaf157bc62dca
Arthur de Souza Possebon <arthur.possebon@ska.com.br>
2024-07-19 16:33:15 -0300

    listaCompras

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ListaCompras from './components/ListaCompras';
+
+function App() {
+  return (
+    <div className="body">
+        <ListaCompras />
+    </div>
+  );
+}
+
+export default App;
