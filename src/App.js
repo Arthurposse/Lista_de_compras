46b42643403a8fc4122af1dbe671473d0f9c46ca
Arthur de Souza Possebon <arthur.possebon@ska.com.br>
2024-07-20 20:21:36 -0300

    listaCompras_20.07

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
-import './App.css';
-import ListaCompras from './components/ListaCompras';
+import "./App.css";
+import ListaCompras from "./components/ListaCompras";
+import React, { useState } from "react";
+import Carrinho from "./assets/Carrinho_compras.png";
 
 function App() {
+  const [textoItem, setTextoItem] = useState("");
+  const [textoQuant, setTextoQuant] = useState("");
+  const [itens, setItens] = useState([]);
+
+  const atualizarItem = (event) => {
+    setTextoItem(event.target.value);
+  };
+
+  const atualizarQuant = (event) => {
+    setTextoQuant(event.target.value);
+  };
+
+  const adicionarItem = () => {
+    if (textoItem && textoQuant) {
+      setItens([...itens, { item: textoItem, quant: textoQuant }]);
+      setTextoItem("");
+      setTextoQuant("");
+    }
+  };
+
+  const excluirItem = (index) => {
+    const novosItens = itens.filter((_, i) => i !== index);
+    setItens(novosItens);
+  };
+
   return (
     <div className="body">
-        <ListaCompras />
+      <div className="parte_esquerda">
+        <img src={Carrinho} alt="Carrinho de compras" />
+
+        <h1> Lista de Compras </h1>
+
+        <h3> Item: </h3>
+        <input
+          type="text"
+          placeholder="Insira o nome do item"
+          value={textoItem}
+          onChange={atualizarItem}
+        />
+
+        <h3> Quantidade: </h3>
+        <input
+          type="number"
+          placeholder="Insira a quantidade"
+          value={textoQuant}
+          onChange={atualizarQuant}
+        />
+
+        <button onClick={adicionarItem}>Adicionar</button>
+      </div>
+
+      <div className="parte_direita">
+        <h1> Sua lista: </h1>
+
+        <div className="Lista">
+          {itens.map((item, index) => (
+            <ListaCompras key={index} item={item.item} quant={item.quant} onExcluir={() => excluirItem(index)} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/ListaCompras.jsx
+++ src/components/ListaCompras.jsx
@@ -1,56 +1,16 @@
 import React from "react";
-import "../ListaCompras.css";
-import Carrinho from '../assets/Carrinho_compras.png'
+import "../App.css";
 
-function ListaCompras() {
-    function adicionar() {
-        const input_item = document.querySelector('.input_item');
-        const input_quant = document.querySelector('.input_quant');
-        const Lista = document.getElementById('Lista');
+function ListaCompras({ item, quant, onExcluir }) {
+  let texto = `${item}: ${quant} unidade(s)`;
 
-        if(input_item.value === '' && input_quant.value === '') {
-            alert('Insira os dados!!')
-        }
-
-        else if(input_item.value === '') {
-            alert('Insira o nome do item!!')
-        }
-        else if (input_quant.value === '') {
-            alert('Insira a quantidade!!')
-        }
-        else {
-            Lista.innerHTML += `<li> ${input_item.value}: ${input_quant.value} unidade(s) </li>`;
-            input_item.value = '';
-            input_quant.value = '';
-        }
-    }
-
-    return (
-        <div className="main">
-            <div className="parte_esquerda">
-
-                <img src={Carrinho} alt="Carrinho de compras" />
-
-                <h1> Lista de Compras </h1>
-
-                <h3> Item: </h3>
-                <input type="text" className="input_item" placeholder="Insira o nome do item" />
-
-                <h3> Quantidade: </h3>
-                <input type="number" className="input_quant" placeholder="Insira a quantidade" />
-
-                <button onClick={adicionar}> Adicionar </button>
-            </div>
-
-            <div className="parte_direita">
-                <h1> Sua lista: </h1>
-
-                <ul id="Lista">
-
-                </ul>
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="item">
+      <h3>{texto}</h3>
+      <button>Editar</button>
+      <button onClick={onExcluir}>Excluir</button>
+    </div>
+  );
+}
 
 export default ListaCompras;
